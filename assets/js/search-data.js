c041861c5fb969ce10741f9e91cb9615b70b68b6
emanuelegent <94073032+emanuelegent@users.noreply.github.com>
2025-08-15 17:21:54 +0000

    Deploying to gh-pages from @ emanuelegent/emanuelegent.github.io@a37c4c1e04ea15553987074e89d19d9dc2f670a2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-my-latest-paper-modelling-intense-storms-hitting-the-uk-and-europe-with-the-latest-km-scale-gfdl-climate-model-is-out",
+          title: 'My latest paper modelling intense storms hitting the Uk and Europe with the...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
